694c59cc697a80e8bdec8f5d6f2a9370be409c6a
mijanur-rahman-40 <mijanurrahman31416@gmail.com>
2022-11-06 02:23:26 +0600

    Updated..

--- lamports_vector_clock.cpp
+++ lamports_vector_clock.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <conio.h>
 #define SIZE 10
 using namespace std;
 
---
+++ test.js
@@ -0,0 +1,5 @@
+const prompt = require("prompt-sync")();
+
+let a = prompt("Enter a number: ")
+console.log(a)
+
